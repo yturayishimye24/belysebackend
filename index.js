477933dfc0a96284ee9d2a8f3985eaa8191aa4c3
yturayishimye24 <yturayishimye@gmail.com>
2026-09-19 16:13:35 +0200

    Updated backend

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   methods: ["POST", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
 }));
